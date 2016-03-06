374fb3bc7a4ded9f96cb8757f60eaab6c3825a30
Christopher Peters <cpeter9@gmail.com>
2016-03-06 08:54:53 -0600

    Add a few tests to filter and index

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -1,7 +1,34 @@
 context("filter.R")
 
+first_available_index <- indices()[[1]]
 
-test_that("functions return the correct classes", {
+test_that("filter", {
   expect_true("filtered" %in% class(filter(NULL)))
 })
+
+test_that("ids", {
+  expect_true("comb" %in% class(ids_(c(1, 2))))
+  expect_true("ids" %in% class(ids_(c(1, 2))[[1]]))
+
+
+  expect_equal(index(first_available_index) %>%
+                 filter() %>%
+                 ids(c(1, 2)) %>%
+                 exec() %>%
+                 n(),
+               1)
+
+  expect_equal(index(first_available_index) %>%
+                 filter() %>%
+                 ids(c(1, 2)) %>%
+                 exec() %>%
+                 n(),
+               2)
+})
+
+test_that("ids_", {
+  expect_true("comb" %in% class(ids_(c(1, 2))))
+  expect_true("ids" %in% class(ids_(c(1, 2))[[1]]))
+})
+
 
