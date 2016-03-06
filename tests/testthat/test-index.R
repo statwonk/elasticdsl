8975e19e10d90398a759f0921387bd3c2db9f4e9
Christopher Peters <cpeter9@gmail.com>
2016-03-06 09:17:08 -0600

    Add a few tests to filter and index, one showing that id_ fails for a single id

--- tests/testthat/test-index.R
+++ tests/testthat/test-index.R
@@ -1,12 +1,19 @@
 context("index.R")
 
+first_available_index <- indices()[[1]]
+
 
 test_that("index() returns the right class", {
   # Using the ES_IP env var to pass in the
   # cluster's ip.
-  expect_equal(class(index("gbif")), "index")
+  expect_equal(class(index(first_available_index)), "index")
 })
 
 test_that("indices returns at least one index", {
   expect_gt(length(indices()), 0)
+})
+
+test_that("get_map", {
+  expect_equal(names(elasticdsl:::get_map(first_available_index)), "record")
+  expect_equal(names(elasticdsl:::get_map(first_available_index)[["record"]]), "properties")
 })
